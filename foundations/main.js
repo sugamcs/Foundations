7422c6d193905aef79392e4e532bd50fb0b2ae7e
sugamcs <sugambhargav1020@gmail.com>
2023-08-15 23:37:20 +0530

    Fixed some code issues on the rock paper scissors exercise

--- foundations/main.js
+++ foundations/main.js
@@ -1,26 +1,31 @@
 function getComputerChoice(){
         choices = ["Rock", "Paper", "Scissors"];
-        const randomIndex = Math.floor(Math.random() * choices.length);
+        randomIndex = Math.floor(Math.random() * choices.length)
         return choices[randomIndex]
 }
+        
 
-const computerChoice = getComputerChoice() 
-
-
-function round(playerSelection,computerSelection){
-        computerSelection = computerChoice
-        if (playerSelection == computerChoice){
-                return "Tie"
-        }else if(playerSelection.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper" ) {
-                return "You lose(paper,rock)"
-        }else if(playerSelection.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" ) {
-                return "You win(rock,scissors)"
-        }else if(playerSelection.toLowerCase() === "paper" && computerChoice.toLowerCase() ==="rock" ) {
-                return "You win(paper,rock)"
-        }else if (playerSelection.toLowerCase() === "scissors"&& computerChoice.toLowerCase() === "paper") {
-                return "You win(scissors,paper)"
-        }else if (playerSelection.toLowerCase() === "scissors"&& computerChoice.toLowerCase() === "rock") {
-                return "You lose(scissors,rock)"
-        }
+function playRound(playerSelection,computerSelection) {
+        computerChoice = computerSelection
+        if (playerSelection == computerChoice) {
+                return "Tie";
+            } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
+                return "You lose(paper beats rock)";
+            } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+                
+                return "You win(rock beats scissors)";
+            } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+                
+                return "You win(paper beats rock)";
+            } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+            
+                return "You win(scissors beats paper)";
+            } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
+                return "You lose(rock beats scissors)";
+            }
 }
-console.log(round("rock", computerChoice))
+
+let computerSelection = getComputerChoice()
+console.log(playRound("Rock",computerSelection))
+       
+     
