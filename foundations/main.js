e6b96bb0f54e226f6026ffaa4eb8dac744cd6fc8
sugamcs <sugambhargav1020@gmail.com>
2023-08-15 12:05:08 +0530

    Basic Rock Paper Scissors game exercise

--- foundations/main.js
+++ foundations/main.js
@@ -1,6 +1,26 @@
-function capitalize(string) {
-
-    return string[0].toUpperCase() + string.slice(1).toLowerCase()
+function getComputerChoice(){
+        choices = ["Rock", "Paper", "Scissors"];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex]
 }
 
-console.log(capitalize('hi MY namE Is JoHn doE'))
+const computerChoice = getComputerChoice() 
+
+
+function round(playerSelection,computerSelection){
+        computerSelection = computerChoice
+        if (playerSelection == computerChoice){
+                return "Tie"
+        }else if(playerSelection.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper" ) {
+                return "You lose(paper,rock)"
+        }else if(playerSelection.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" ) {
+                return "You win(rock,scissors)"
+        }else if(playerSelection.toLowerCase() === "paper" && computerChoice.toLowerCase() ==="rock" ) {
+                return "You win(paper,rock)"
+        }else if (playerSelection.toLowerCase() === "scissors"&& computerChoice.toLowerCase() === "paper") {
+                return "You win(scissors,paper)"
+        }else if (playerSelection.toLowerCase() === "scissors"&& computerChoice.toLowerCase() === "rock") {
+                return "You lose(scissors,rock)"
+        }
+}
+console.log(round("rock", computerChoice))
