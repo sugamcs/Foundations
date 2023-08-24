886c0b7b738d7379b5fc59c86d9260170a852864
sugamcs <sugambhargav1020@gmail.com>
2023-08-24 15:28:18 +0530

    Finished all the javascript exercises in fundamentals part 4

--- foundations/main.js
+++ foundations/main.js
@@ -1,63 +1,19 @@
-function getComputerChoice(){
-        choices = ["Rock", "Paper", "Scissors"];
-        randomIndex = Math.floor(Math.random() * choices.length)
-        return choices[randomIndex]
-}
-        
+// celsius to farenheit :
+// C = (X * 9/5 + 32) F
 
-function playRound(playerSelection,computerSelection) {
-        computerChoice = computerSelection
-        if (playerSelection == computerChoice) {
-                return "Tie";
-            } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-                return "You lose(paper beats rock)";
-            } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-                
-                return "You win(rock beats scissors)";
-            } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-                
-                return "You win(paper beats rock)";
-            } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-            
-                return "You win(scissors beats paper)";
-            } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-                return "You lose(rock beats scissors)";
-            }
+// farenheit to celsius:
+// F =  (x-32) * 5/9 C
+
+function convertToCelsius(tempInFarenheit){
+    celsius = (tempInFarenheit - 32) * 5/9
+    return `Temperature is ${celsius.toFixed(1)} degree celsius.`
 }
 
-let computerSelection = getComputerChoice()
-console.log(playRound("Rock",computerSelection))
-       
+function convertToFarenheit(tempInFarenheit){
+    farenheit = (tempInFarenheit * 9/5 + 32)
+    return `Temperature is ${farenheit.toFixed(1)} degree farenheit.`
+}
 
-function game() {
-        let computerWins = 0;
-        let playerWins = 0;
-    
-        for (let round = 0; round < 5; round++) {
-            const playerChoice = prompt("What is your choice? ");
-            if (!playerChoice) {
-                console.log("Invalid input. Please enter 'rock', 'paper', or 'scissors'.");
-                continue; 
-            }
-    
-            const computerChoice = getComputerChoice();
-            const roundResult = playRound(playerChoice, computerChoice);
-            console.log(roundResult);
-    
-            if (roundResult.includes("win")) {
-                playerWins++;
-            } else if (roundResult.includes("lose")) {
-                computerWins++;
-            }
-        }
-    
-        if (playerWins > computerWins) {
-            console.log("You win the game!");
-        } else if (computerWins > playerWins) {
-            console.log("Computer wins the game!");
-        } else {
-            console.log("It's a tie game!");
-        }
-    }
-game();
-    
+
+console.log(convertToCelsius(32))
+console.log(convertToFarenheit(0))
